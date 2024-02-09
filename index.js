4fe7ab8fa086010392295a3e809644abdf9e3b82
vipin10100001 <75900544+vipin10100001@users.noreply.github.com>
2024-02-09 20:17:30 +0530

    creating a server - commit 1

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const server = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+});
+
+server.on('error', (error) => {
+  console.error('Server error:', error);
+});
+
+server.listen(9080, () => {
+  console.log('Server is listening on port 9080');
+  console.log("hello world!");
+});
